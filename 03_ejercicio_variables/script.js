f9c0d380c8bd476ea625b91130f858fe5cd4270d
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-03-12 09:30:19 -0300

    ok

--- 03_ejercicio_variables/script.js
+++ 03_ejercicio_variables/script.js
@@ -1,12 +1,42 @@
 //creacion de variables
-const rut = 226311645; //constante
-let numero = 0; //numerica
+console.log("todo bien");
+
+
+function mostrarDecimal() {
+    alert(decimal);
+}
+
+function mostrarArray() {
+    alert("array is: \n" + array);
+}
+
+function mostrarMatriz() {
+    alert("Matriz is: \n" + matriz);  
+}
+
+function mostrarBool(){
+    alert(boolean);
+}
+
+function mostrarIndefinida(){
+    alert(indefinida);
+}
+
+function mostrarText(){
+    alert(text);
+}
+
+function mostrarNum(){
+    alert(num);
+}
+
+
+
+let num = 0; //numerica
 let decimal = 2.5; //float
-let texto = "Hola"; //string
-let booleano = true; //bool
-
-let indefinida = underfined; //indefinida
-
+let text = "Hola"; //string
+let boolean = true; //bool
+let indefinida = undefined; //indefinida
 let array = [1, "Hola" ,25]; //lista de elementos
 let matriz = [[1,2],[3,4]]; //lista dentro de lista
 let objeto = {nombre: "Dany", edad:18}; //objeto blablabla
---
+++ 1.2_ejercicios_uso_variables/script.js
@@ -0,0 +1,75 @@
+console.log("Vinculado correctamente...")
+// Ejercicio 1: Declaración de variables
+// Instrucciones: Declara dos variables numéricas llamadas 'ahora' y 'fecha_nac'.
+// Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
+
+function declararVariables() {
+    alert("Esto es una alerta");
+    // Tu código aquí
+
+    let ahora = parseInt (prompt ("Ingresar Año Actual"));
+    alert (ahora)
+
+    let fecha_nac = parseInt (prompt ("Año De Nacimiento"));
+    let edad = ahora - fecha_nac;
+    alert ("Edad es: " + ahora - fecha_nac + ",")
+
+    var1 = var2;
+
+    }
+    
+    // Ejercicio 2: Asignación de valores
+    // Instrucciones: Completa la función para asignar el valor de 'var1' a 'var2' y luego muestra el valor de 'var2'.
+    function asignarValores() {
+    // Tu código aquí
+
+    let var1 = promp("Ingrese un valor: ");
+    let var2 = var1;
+
+    alert("El valor de var2 es" + var2);
+
+    }
+    
+    // Ejercicio 3: Operaciones matemáticas
+    // Instrucciones: Completa la función para realizar las siguientes operaciones:
+    // Suma 'num1','num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
+    // Muestra los resultados en un mensaje.
+    function realizarOperaciones() {
+    
+        let num1 = parseInt (promp("Ingrese Num1: "))
+        let num2 = parseInt (promp("Ingrese Num2: "))
+        let num3 = parseInt (promp("Ingrese Num3: "))
+
+        let suma = num1 + nm2 +num3;
+        let resta = num1 - num2 - num3;
+        let multiplica = num2 + num3;
+        let divide = num1 / 120;
+
+        alert ("resultados: \n Suma: " + suma "\n Resta:" + resta + "\n Multiplicacion:" + multiplica + "\n Divicion:" + divicion);
+    alert( );
+    
+    // Tu código aquí
+    }
+    
+    // Ejercicio 4: Manipulación de cadenas
+    // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'n_casa'.
+    // Luego, muestra la longitud del mensaje y el antepenultimo en el mensaje. (.length)
+    function manipularCadenas() {
+
+    // Tu código aquí
+    }
+    let direccion = prompt ("Ingrese su direccion:");
+    let n_casa = prompt ("Ingrese su direccion:");
+    let direccioncompleta = direccion + " " + nCasa;
+
+    alert ("la direccion es:" + direccion + "\n cantidad de caracteres: " + mensaje.lengh() "Caracter ante penultimo: " + mensaje [mensaje.lengh-3]);
+
+
+    //esto lo borro despues
+console.log("funcionandocorrectamente...")
+
+function declararvariable(){
+    let ahora = parseInt (prompt ("Ingresar Año Actual"));
+    alert (ahora); 
+}
+
