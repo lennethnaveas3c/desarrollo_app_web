f3e7b9079354fa9baea4b15efbdb09141c45be9b
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-03-19 09:31:04 -0300

    ok

---
+++ 06_condicionales/script.js
@@ -0,0 +1,21 @@
+//creacion de variables
+console.log("todo bien");
+
+function MostrarTemperatura(){
+
+let temperatura = 27;
+if (temperatura <= 0) {
+    alert("hace frio");
+} else {
+    if (temperatura >=0 + 1 && temperatura <=25) {
+        alert("la temperatura es agradable");
+    }
+    if (temperatura >=25) {
+        alert("hace calor");
+    }
+}
+}
+
+function MostrarUsuario(){
+    let 
+}
