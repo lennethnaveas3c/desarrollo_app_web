cbc809217fd9e8777e14aa342aff50732494f8f2
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-03-20 11:15:14 -0300

    ok

--- 07_operadores_logicos/script.js
+++ 07_operadores_logicos/script.js
@@ -3,28 +3,17 @@ console.log("todo bien");
 
 
 //ejercicio 5
-function MostrarTemperatura(){
-
-let temperatura = 27;      //temperatura
-if (temperatura <= 0) {    //si la emperatura es meno o igual a 0
-    alert("hace frio");
-} else {
-    if (temperatura >=0 + 1 && temperatura <=25) {  //si la temperatura es mayor o igual a 0 y menor o igual a 25
-        alert("la temperatura es agradable");
-    }
-    if (temperatura >=25) {   //si la temperatura es mayor o igual a 25
-        alert("hace calor");
-    }
-}
+function MostrarVerificacion(){
+    let 
 }
 
 //ejercicio 6
-function MostrarUsuarioContraseña(){
-    let Usuario = "usuario123"; //nombre del usuario
-    let Contraseña = "secreto"; //nombre de la contraseña
+function MostrarDescuento(){
+    let Usuario = "usuario123";
+    let Contraseña = "secreto"; 
     let respuesta_Usuario = prompt("Ingrese su nombre de usuario");
-    let respuesta_Contraseña = prompt("Ingrese su contraseña"); //para poder ingresar un dato
-    if (Usuario == respuesta_Usuario && Contraseña == respuesta_Contraseña) { //el dato correspondiente a cada preguna
+    let respuesta_Contraseña = prompt("Ingrese su contraseña"); 
+    if (Usuario == respuesta_Usuario && Contraseña == respuesta_Contraseña) { 
         alert("Acceso concedido")
     } else {
             alert("Acceso denegado")
@@ -32,30 +21,35 @@ function MostrarUsuarioContraseña(){
 }
 
 //ejercicio 7
-function MostrarSigno() {
-    let numero = parseInt(prompt("Introduce un numero")); //introducir un numero
+function MostrarValidacion() {
+    let numero = parseInt(prompt("Introduce un numero")); 
     if (numero < 0) {
-        alert("El numero es negativo"); //alerta numero negativo
+        alert("El numero es negativo"); 
     } else if (numero > 0) {
-        alert("El numero es positivo"); //alerta numero positivo
+        alert("El numero es positivo"); 
     } else {
-        alert("El numero es cero"); //alerta numero 0
+        alert("El numero es cero"); 
     }
 }
 
 
 //ejercicio 8
-function MostrarPuntuacion() {
-    let puntuacion = parseInt(prompt("Introduce tu puntuación:")); //puntuacion
-    if (puntuacion >= 90) {     //puntuacin mayor o igual a 90
+function MostrarCondiciones() {
+    let puntuacion = parseInt(prompt("Introduce tu puntuación:"));
+    if (puntuacion >= 90) {     
         alert("Excelente");
-    } else if (puntuacion >= 70 && puntuacion < 90) { //puntuacion mayor a 70 y menor a 90
+    } else if (puntuacion >= 70 && puntuacion < 90) { 
         alert("Buen trabajo");
     } else {
         alert("Necesitas mejorar");
     }
 }
 
+
+//ejercicio 9
+function MostrarRestringido() {
+
+}
 
 
 
