073b8ea3d049f6f7d56f392c7eb246334dbfadf4
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-03-25 10:58:57 -0300

    ok

---
+++ 08_ejercicios_basicos/script.js
@@ -0,0 +1,25 @@
+//creacion de variables
+console.log("todo bien");
+
+
+//ejercicio 14
+function MostrarParImpar() {
+    let numero = parseInt(prompt("Introduce un numero")); 
+    if (numero % 2 == 0){
+        alert("El numero es par");
+    } else {
+        alert("El numero es impar");
+    }
+}
+
+
+//ejercicio 15
+function MostrarIndiceMasa() {}
+
+
+//ejercicio 16
+function MostrarNotas() {}
+
+
+
+
