8b6c67e7c06c079510bb3c682cdaabeac7233e41
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-04-09 08:35:39 -0400

    ok

---
+++ js/11_practicarJS/script.js
@@ -0,0 +1,70 @@
+//creacion de variables
+console.log("todo bien");
+
+
+//ejercicio 1:
+function Mostrar() {}
+
+
+//ejercicio 2:
+function Mostrar() {}
+
+
+//ejercicio 3:
+function Mostrar() {}
+
+
+//ejercicio 4:
+function Mostrar() {}
+
+
+//ejercicio 5:
+function Mostrar() {}
+
+
+//ejercicio 6:
+function Mostrar() {}
+
+
+//ejercicio 7:
+function Mostrar() {}
+
+
+//ejercicio 8:
+function Mostrar() {}
+
+
+//ejercicio 9:
+function Mostrar() {}
+
+
+//ejercicio 10:
+function Mostrar() {}
+
+
+//ejercicio 11:
+function Mostrar() {}
+
+
+//ejercicio 12:
+function Mostrar() {}
+
+
+//ejercicio 13:
+function Mostrar() {}
+
+
+//ejercicio 14:
+function Mostrar() {}
+
+
+//ejercicio 15
+function Mostrar() {}
+
+
+//ejercicio 16:
+function Mostrar() {}
+
+
+//ejercicio 17:
+function Mostrar() {}
