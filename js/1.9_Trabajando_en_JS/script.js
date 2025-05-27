56428204b9d7ba7d7abbd807fa90588129b285d2
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-05-27 11:13:14 -0400

    ok

--- js/1.9_Trabajando_en_JS/script.js
+++ js/1.9_Trabajando_en_JS/script.js
@@ -4,26 +4,21 @@ console.log("todo bien");
 
 //ejercicio 1:
 function MostrarRegistro() {
-    let n = parseInt(prompt("Ingrese un numero:"));
-    let array = [];
-    let suma = 0;
-    for(let i = 1; i <= n; i++){
-        array.push(i);
-        suma += i;
+    let participantes = [];
+    let nombre = prompt("Intoduce un nombre: (escribe 'salir' para terminar el registro")
+    while (nombre != "salir"){
+        participantes.push(nombre);
+        nombre = prompt("Introduce un nombre: (escribe 'salir' para terminar el registro)");
     }
-    alert(`La suma de los primeros ${n} numeros naturales es:\n ${suma}`);
-    alert(`Los primeros ${n} numeros naturales son:\n ${array.join(" -")}`);
+    if (participantes.length > 5) {
+        console.log ("tenemos mas de 5 participantes");
+    }
+    alert(`se han registrado ${participantes.length} participantes`);
+    console.log(`participantes: ${participantes.join(", ")}`);
 }
 
 
 //ejercicio 2:
 function MostrarCaja() {
-    let array = [];
-    for(let n = 1; n <= 200; n++){
-        if(n % 2 == 0){
-            array.push(n);
-        }
-    }
-    alert(`Los primeros 100 numeros pares son:\n ${array.join(" -")}`);
 }
 
