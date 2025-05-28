f66edfcd9c430d5ae869d384bbf0fe661b301582
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-05-28 09:19:08 -0400

    ok

---
+++ js/1.9.1 Ejercicios Aritméticos/script.js
@@ -0,0 +1,32 @@
+//creacion de variables
+console.log("todo bien");
+
+
+//ejercicio 1:
+function MostrarPromedios() {
+    let notas = [];
+    let suma = 0;
+    for (let i = 0; i < 5; i++) {
+        let nota = parseFloat(prompt("Introduce una nota: "));
+        if (!isNaN(nota) || nota < 1 || nota > 7) {
+            notas.push(nota);
+            suma += nota;
+        } else {
+            alert("nota ivalida, porfavor ingrese un numero entre el 1.0 al 7.0")
+            i--;
+        }
+    }
+    let promedio = suma / notas.length;
+    alert(`La nota promedio es ${promedio.toFixed(2)}`);
+    if (promedio >= 4.0) {
+        alert("Aprobado");
+    } else {
+        alert("Reprobado");
+    }
+}
+
+
+//ejercicio 2:
+function MostrarParYInpar() {
+}
+
