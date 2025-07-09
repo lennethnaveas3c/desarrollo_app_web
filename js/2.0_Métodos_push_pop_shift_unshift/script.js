bbd99aad84ba7ffd3138eeee466a24788a30d8e4
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-07-09 09:29:42 -0400

    ok

--- js/2.0_Métodos_push_pop_shift_unshift/script.js
+++ js/2.0_Métodos_push_pop_shift_unshift/script.js
@@ -11,7 +11,7 @@ function agregarElementoCompra() {
 }
 */
 
-
+/*
 //ejemplo 1 
 
 const resultado = document.getElementById("resulado");
@@ -33,4 +33,61 @@ function agregarElemento2() {
     let elemento = document.getElementById("lista2").value;
     carrito.push(elemento)
     documento.getElementById(resultado2).innerTextHTML = Carro: `${carrito.join(" - ")}`;
+}
+*/
+
+/*
+🧠 Ejercicio 1: Agregar elementos
+Crea un arreglo vacío llamado compras.
+Usa push() para agregar los siguientes elementos:
+"pan", "leche" y "huevos".
+Muestra el arreglo resultante  en html.
+*/
+// referencia ul
+
+//ejercicio 1
+const resultado = document.getElementById("resultado");
+function agregarElementoCompra(){
+     //valor input
+    let elemento = document.getElementById("lista").value;
+    //Crear elemento HTML --> CreateElement
+    let item = document.createElement("li");
+    item.innerText = elemento; //<li>--InnerText--</li>
+    //appendChild inserta valor hijo en Padre
+    // resultado(<ul>(item(<li></li>))</ul>)
+    resultado.appendChild(item);
+}
+
+//ejercicio 2
+let carrito = [];
+function mostrarListaCompra(){
+    const listta = document.getElementById("resultado2");
+    lista.innerHTML = ""; //limpiar antes de mostrar
+
+    for (let i = 0; i < carrito.lencht; i++){
+        const li = document.createElement("li");
+        li.className = "list-group-item d-flex justify-content-between align-items-center mb-2";
+        li.textContent = `-${carrito[i]}`;
+        lista.appendChild(li);
+        mostrarListaCompra()
+    }
+}
+
+function agregarElemento2() {
+    const input = document.getElementById("lista2");
+    const valor = input.value.trim(); //guardo valor sin espacios extras
+
+    if (valor === "") return;
+    carrito.push(valor); //agregar valor al final
+    
+    input.value = "";
+    input.focus(); //agrega foco hacia el input
+    mostrarListaCompra()
+}
+
+function eliminarUltimoElemento() {
+    if (carrito.length > 0){
+        carrito.pop();
+        mostrarListaCompra()
+    }
 }
