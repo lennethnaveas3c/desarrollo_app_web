56037270b2a8549a9c78892b0ee244f61ca7411a
lennethnaveas3c <lennethnaveas@liceovvh.cl>
2025-07-28 10:42:32 -0400

    ok

--- js/3.2_MeGustaParte2/script.js
+++ js/3.2_MeGustaParte2/script.js
@@ -1,5 +1,21 @@
-function mostrarMeGusta() {
-    const contador = document.getElementById("likes");
+function mostrarMeGusta1() {
+    const contador = document.getElementById("likes1");
+    let texto = contador.textContent;
+    let numero = parseInt(texto);
+    numero++;
+    contador.textContent = `${numero} like(s)`;
+}
+
+function mostrarMeGusta2() {
+    const contador = document.getElementById("likes2");
+    let texto = contador.textContent;
+    let numero = parseInt(texto);
+    numero++;
+    contador.textContent = `${numero} like(s)`;
+}
+
+function mostrarMeGusta3() {
+    const contador = document.getElementById("likes3");
     let texto = contador.textContent;
     let numero = parseInt(texto);
     numero++;
